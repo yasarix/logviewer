38740b5f634793168faedf10b68780c93a4a2251
Yasar Senturk <yasar@senturk.name.tr>
2014-04-01 00:12:54 -0400

    Adding Configuration class

---
+++ LogViewer/Configuration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace LogViewer
+{
+	public class Configuration
+	{
+		string FilePath = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "/config.xml";
+
+		public string this[string key]
+		{
+			get
+			{
+				var document = XDocument.Load(FilePath);
+				var values = from n in document.Root.Elements()
+						where n.Name == key
+						select n.Value;
+
+				if(values.Any()) {
+					return values.First();
+				}
+
+				return null;
+			}
+			set
+			{
+				var document = XDocument.Load(FilePath);
+				var values = from n in document.Root.Elements()
+						where n.Name == key
+						select n;
+
+				if(values.Any()) {
+					values.First().Value = value;
+				}
+				else {
+					document.Root.Add(new XElement(key, value));
+				}
+
+				document.Save(FilePath);
+			}
+		}
+
+		public Configuration ()
+		{
+		}
+	}
+
+}
